b06d55b64736b15f7faf99fadc22fecd7b66ccf8
AChubatova <64786092+AChubatova@users.noreply.github.com>
2020-07-28 15:05:05 +0200

    setcommit4

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -39,7 +39,7 @@ object CommandMaster : BuildType({
 
     steps {
         powerShell {
-            name = "step1bla"
+            name = "step2bla"
             scriptMode = file {
                 path = "output3_master.ps1"
             }
